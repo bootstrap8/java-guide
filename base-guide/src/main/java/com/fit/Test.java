e3e82ccf67e07ff891b4a4461bb13d8ec11ef486
zhaoyong.8@163.com <Zhy@123>
2020-07-10 10:14:41 +0800

    xx

--- base-guide/src/main/java/com/fit/Test.java
+++ base-guide/src/main/java/com/fit/Test.java
@@ -1,9 +1,12 @@
 package com.fit;
 
+import com.alibaba.fastjson.JSON;
 import com.google.common.base.Joiner;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Map;
 
 /**
  * @author Stone (300482)
@@ -15,5 +18,19 @@ public class Test {
         String[] columns = new String[]{"apple", "oppo", "vivo"};
         System.out.println(Joiner.on(",").join(columns));
         System.out.println(new SimpleDateFormat("yyyyMMdd").format(new Date()));
+
+
+
+        long time = System.currentTimeMillis() / 1000 - 3 * 24 * 60 * 60;
+        Date date = new Date(time * 1000);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(time * 1000);
+        System.out.println(calendar.get(Calendar.YEAR));
+
+
+        String json = "{}";
+
+        Map map = JSON.parseObject(json, Map.class);
+        System.out.println(map);
     }
 }
